60019841d20a7b4d722c1ff7741dd71b3e385b60
Jaewook Lee <wookboy00@naver.com>
2024-09-03 20:36:06 +0900

    Chore: valid check when register to saga registry

--- registry.go
+++ registry.go
@@ -2,6 +2,7 @@ package saga
 
 import (
 	"errors"
+	"log"
 	"sync"
 )
 
@@ -13,11 +14,21 @@ func NewRegistry[Tx TxContext](orchestrator Orchestrator[Tx]) *Registry[Tx] {
 	}
 }
 
-func RegisterSagaTo[S Session, Tx TxContext](r *Registry[Tx], s Saga[S, Tx]) {
+func RegisterSagaTo[S Session, Tx TxContext](r *Registry[Tx], s Saga[S, Tx]) error {
+	if s.Name() == "" {
+		return errors.New("saga name is empty")
+	}
+
+	if r.HasSaga(s.Name()) {
+		return errors.New("saga already registered")
+	}
+
 	r.mutex.Lock()
 	defer r.mutex.Unlock()
 
 	r.sagas = append(r.sagas, convertSaga(s))
+
+	return nil
 }
 
 type Registry[Tx TxContext] struct {
@@ -47,6 +58,7 @@ func (r *Registry[Tx]) consumeMessage(packet messagePacket) error {
 
 	for _, f := range orchestrations {
 		if err := f(); err != nil {
+			log.Print("Error in orchestrating: ", err)
 			return err
 		}
 	}
@@ -55,6 +67,14 @@ func (r *Registry[Tx]) consumeMessage(packet messagePacket) error {
 }
 
 func (r *Registry[Tx]) StartSaga(sagaName string, sessionArgs map[string]interface{}) error {
+	if sagaName == "" {
+		return errors.New("saga name is empty")
+	}
+
+	if sessionArgs == nil {
+		return errors.New("session args is nil")
+	}
+
 	r.mutex.Lock()
 	defer r.mutex.Unlock()
 
@@ -70,5 +90,19 @@ func (r *Registry[Tx]) StartSaga(sagaName string, sessionArgs map[string]interfa
 		return errors.New("saga not found")
 	}
 
+	log.Print("Starting saga: ", sagaName)
+
 	return r.orchestrator.StartSaga(*target, sessionArgs)
+}
+
+func (r *Registry[Tx]) HasSaga(sagaName string) bool {
+	r.mutex.Lock()
+	defer r.mutex.Unlock()
+	for _, s := range r.sagas {
+		if s.Name() == sagaName {
+			return true
+		}
+	}
+
+	return false
 }
